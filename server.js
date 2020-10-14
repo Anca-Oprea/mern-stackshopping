6bd5fb38535a05c46e22cf21d5849139ff5622a2
Anca-Oprea <opreaanca1987@gmail.com>
2020-10-14 11:57:44 +0200

    newone

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	// });
 
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, './client/build')));
 
 }
 
