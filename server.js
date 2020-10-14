75bb20e0e2b36b60e50ecfb1b33826c14804a5e4
Anca-Oprea <opreaanca1987@gmail.com>
2020-10-14 23:54:22 +0200

    now

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(express.static(path.join(__dirname, './client/build')));
 }
 
-const port = process.env.Port || 5000;
-app.listen(port, () => console.log(`Server started on Port ${port}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server started on port ' + port));
