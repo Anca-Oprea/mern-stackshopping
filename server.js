bda71d1366459f8112f93e60b27dad95f1ea1a5b
Anca-Oprea <opreaanca1987@gmail.com>
2020-10-15 07:57:05 +0200

    try

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require("passport");
 const users = require("./routes/api/users.js");
 const app = express();
+const path = require('path');
 
 //Bodyparser Middleware
 app.use(bodyParser.urlencoded({
